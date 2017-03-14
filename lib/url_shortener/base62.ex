787d699897b0182ac9cee86268b7333a385423df
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-03-15 02:49:13 +0900

    Add documentation to Base62 module
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- lib/url_shortener/base62.ex
+++ lib/url_shortener/base62.ex
@@ -3,38 +3,35 @@ defmodule UrlShortener.Base62 do
            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-  def encode(s) do
+  @moduledoc """
+  Handle Base62 encoding of given string to shorter string. 
+  Decoding is not implemented as it's unnecessary for 
+  this application.
+  """  
+  @doc """
+  Encode given string into shorter Base62 encoded string.
+  Raise ArgumentError when the input is not string.
+  ## Example
+        iex> s = "wovon man nicht sprechen kann, darüber muss man schweigen"
+        iex> UrlShortener.Base62.encode(s)
+        "os1"
+  """
+  def encode(s) when is_binary(s) do
     s
     |> String.to_char_list()
     |> Enum.sum()
     |> encode_helper()
     |> List.to_string()
   end
+  def encode(_), do: raise ArgumentError, "Encoding input should be string type"
 
   defp encode_helper(n) when n < 62, do: [Map.get(encoder_table(), n)]
   defp encode_helper(n) do
     [Map.get(encoder_table(), rem(n, 62)) | encode_helper(div(n, 62))]
   end
 
-  def decode(s) do
-    s_converted_to_int_list = for <<x :: utf8 <- s>> do
-      Map.get(decoder_table(), <<x :: utf8>>)
-    end
-
-    List.zip([s_converted_to_int_list, Enum.to_list(0..byte_size(s))])
-    |> Enum.map(fn {coefficient, power} -> coefficient * (:math.pow(62, power)) end)
-    |> Enum.sum()
-    |> round()
-  end
-
   defp encoder_table() do
     [Enum.to_list(0..61), @base62]
-    |> List.zip()
-    |> Map.new()
-  end
-  
-  defp decoder_table() do
-    [@base62, Enum.to_list(0..61)]
     |> List.zip()
     |> Map.new()
   end
