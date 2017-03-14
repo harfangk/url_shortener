5ca468423303532763aaa14a51d3f2fa07dd441f
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-03-15 02:41:38 +0900

    Remove decode from test
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- test/url_shortener_base62_test.exs
+++ test/url_shortener_base62_test.exs
@@ -5,23 +5,12 @@ defmodule UrlShortener.Base62Test do
   alias UrlShortener.Base62 
 
   test "encode input with non-binary value should raise ArgumentError" do
-    assert_raise(ArgumentError, Base62.encode(35))
-  end
-
-  test "decode input with non-binary value should raise ArgumentError" do
-    assert_raise(ArgumentError, Base62.decode(35))
+    assert_raise ArgumentError, fn ->
+      Base62.encode(35)
+    end
   end
 
   test "encode 'https://www.google.com' should return 'cy'" do
     assert Base62.encode("https://www.google.com") == "cy"
-  end
-
-  test "decode 'LD' should return 'https://www.microsoft.com'" do
-    assert Base62.decode("LD") == "https://www.microsoft.com"
-  end
-
-  test "encoding and decoding should be inverse function" do
-    assert Base62.encode("https://www.facebook.com") |> Base62.decode() == "https://www.facebook.com"
-    assert Base62.encode("KthXBai2") |> Base62.decode() == "KthXBai2"
   end
 end
