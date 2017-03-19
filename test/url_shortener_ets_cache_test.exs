8f8db81377f234259b1c2592dbdcee6e91f940f1
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-03-19 19:26:25 +0900

    Add test for EtsCache
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- test/url_shortener_ets_cache_test.exs
+++ test/url_shortener_ets_cache_test.exs
@@ -4,12 +4,24 @@ defmodule UrlShortener.EtsCacheTest do
 
   alias UrlShortener.EtsCache
 
-  setup do
-    {:ok, cache_process} = UrlShortener.EtsCache.start_link()
-    {:ok, cache_process: cache_process}
+  test "should have cache tables running" do
+    assert is_list :ets.info(:high_priority)
+    assert is_list :ets.info(:medium_priority)
+    assert is_list :ets.info(:low_priority)
   end
 
-  test testdo
+  test ":high_priority should be kept when its size is less than 3000" do
+    :ets.delete_all_objects(:high_priority)
+    initial_size = :ets.info(:high_priority)[:size]
+    EtsCache.renew_cache_tables() 
+    later_size = :ets.info(:high_priority)[:size]
+    assert initial_size == later_size
+  end
 
+  test ":high_priority should be reset when its size is more than 3000" do
+    (1..3001) |> Enum.map(fn(x) -> {x, x} end) |> Enum.map(fn(x) -> :ets.insert(:high_priority, x) end)
+    EtsCache.renew_cache_tables() 
+    :timer.sleep(1)
+    assert :ets.info(:high_priority)[:size] == 0
   end
 end
